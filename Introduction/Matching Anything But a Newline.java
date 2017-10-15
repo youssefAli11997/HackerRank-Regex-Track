7234a893843d731367bc034fac3514833457372e
Youssef Ali <youssefali6212@gmail.com>
2017-10-15 14:16:24 +0200

    Create Matching Anything But a Newline.java

---
+++ Introduction/Matching Anything But a Newline.java
@@ -0,0 +1,11 @@
+// link : https://www.hackerrank.com/challenges/matching-anything-but-new-line
+
+public class Solution {    
+
+    public static void main(String[] args) {
+        
+        Tester tester = new Tester();
+        tester.check("^...\\....\\....\\....$"); 
+    
+    }
+}
