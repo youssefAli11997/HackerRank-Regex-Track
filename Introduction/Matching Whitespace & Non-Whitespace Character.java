6d3cba5861b9de16d5cb9b2690157ef018017f37
Youssef Ali <youssefali6212@gmail.com>
2017-10-17 10:02:41 +0200

    Create Matching Whitespace & Non-Whitespace Character.java

---
+++ Introduction/Matching Whitespace & Non-Whitespace Character.java
@@ -0,0 +1,10 @@
+// link : https://www.hackerrank.com/challenges/matching-whitespace-non-whitespace-character?h_r=next-challenge&h_v=zen
+public class Solution {    
+
+    public static void main(String[] args) {
+        
+        Regex_Test tester = new Regex_Test();
+        tester.checker("\\S{2}\\s\\S{2}\\s\\S{2}"); // Use \\ instead of using \ 
+    
+    }
+}
