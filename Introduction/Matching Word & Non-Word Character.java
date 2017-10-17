01f1a76dfcc63166cbf70e9f6a2621477255f52d
Youssef Ali <youssefali6212@gmail.com>
2017-10-17 10:09:44 +0200

    Create Matching Word & Non-Word Character.java

---
+++ Introduction/Matching Word & Non-Word Character.java
@@ -0,0 +1,10 @@
+// link : https://www.hackerrank.com/challenges/matching-word-non-word?h_r=next-challenge&h_v=zen
+public class Solution {    
+
+    public static void main(String[] args) {
+        
+        Regex_Test tester = new Regex_Test();
+        tester.checker("\\w{3}\\W\\w{10}\\W\\w{3}"); // Use \\ instead of using \ 
+    
+    }
+}
