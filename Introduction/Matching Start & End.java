bc02a9975f719b13aad2a0cd64f1ad749600b231
Youssef Ali <youssefali6212@gmail.com>
2017-10-19 22:16:59 +0200

    Create Matching Start & End.java

---
+++ Introduction/Matching Start & End.java
@@ -0,0 +1,11 @@
+// link : https://www.hackerrank.com/challenges/matching-start-end
+
+public class Solution {    
+
+    public static void main(String[] args) {
+        
+        Regex_Test tester = new Regex_Test();
+        tester.checker("^\\d\\w{4}\\.$"); // Use \\ instead of using \ 
+    
+    }
+}
