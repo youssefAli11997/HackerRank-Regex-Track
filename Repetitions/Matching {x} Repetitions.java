ee38e2da435d8a7fbf9b806fd6a3329b2f9ea6ad
Youssef Ali <youssefali6212@gmail.com>
2017-10-31 14:41:55 +0200

    Create Matching {x} Repetitions.java

---
+++ Repetitions/Matching {x} Repetitions.java
@@ -0,0 +1,11 @@
+// link : https://www.hackerrank.com/challenges/matching-x-repetitions/problem
+
+public class Solution {    
+
+    public static void main(String[] args) {
+        
+        Regex_Test tester = new Regex_Test();
+        tester.checker("^[[a-z][A-Z][02468]]{40}[[13579]\\s]{5}$"); // Use \\ instead of using \ 
+    
+    }
+}
