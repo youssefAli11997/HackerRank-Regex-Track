0b2fb687419d466d0897dc497d4cad0018476884
Youssef Ali <youssefali6212@gmail.com>
2017-11-22 22:44:36 +0200

    Create Matching Zero Or More Repetitions.java

---
+++ Repetitions/Matching Zero Or More Repetitions.java
@@ -0,0 +1,10 @@
+// link : https://www.hackerrank.com/challenges/matching-zero-or-more-repetitions/problem
+public class Solution {    
+
+    public static void main(String[] args) {
+        
+        Regex_Test tester = new Regex_Test();
+        tester.checker("^\\d{2,}[a-z]*[A-Z]*$"); // Use \\ instead of using \ 
+    
+    }
+}
