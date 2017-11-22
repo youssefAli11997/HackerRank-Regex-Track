23fc5712862b9de13f6c7c63a984fc910a9dc6be
Youssef Ali <youssefali6212@gmail.com>
2017-11-22 22:54:32 +0200

    Create Matching Ending Items.java

---
+++ Repetitions/Matching Ending Items.java
@@ -0,0 +1,10 @@
+// link : https://www.hackerrank.com/challenges/matching-ending-items/problem
+public class Solution {    
+
+    public static void main(String[] args) {
+        
+        Regex_Test tester = new Regex_Test();
+        tester.checker("^[a-zA-Z]*s$"); // Use \\ instead of using \ 
+    
+    }
+}
