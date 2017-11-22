7d896c0dc365e936b7fd7065eb0df48dc04dc3be
Youssef Ali <youssefali6212@gmail.com>
2017-11-22 22:47:22 +0200

    Create Matching One Or More Repetitions.java

---
+++ Repetitions/Matching One Or More Repetitions.java
@@ -0,0 +1,10 @@
+// link : https://www.hackerrank.com/challenges/matching-one-or-more-repititions/problem
+public class Solution {    
+
+    public static void main(String[] args) {
+        
+        Regex_Test tester = new Regex_Test();
+        tester.checker("^\\d+[A-Z]+[a-z]+$"); // Use \\ instead of using \ 
+    
+    }
+}
