954a76f2a83d13b7af4f88fb0cd79cef06239864
Youssef Ali <youssefali6212@gmail.com>
2017-11-22 23:16:27 +0200

    Create Matching Word Boundaries.java

---
+++ Matching Word Boundaries.java
@@ -0,0 +1,10 @@
+\\ link : https://www.hackerrank.com/challenges/matching-word-boundaries/problem
+public class Solution {    
+
+    public static void main(String[] args) {
+        
+        Regex_Test tester = new Regex_Test();
+        tester.checker("\\b[aeiouAEIOU][a-zA-Z]*\\b"); // Use \\ instead of using \ 
+    
+    }
+}
