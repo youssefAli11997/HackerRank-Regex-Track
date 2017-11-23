abf37766cb3ba1040fbda1fbb846c219d8be22be
Youssef Ali <youssefali6212@gmail.com>
2017-11-23 08:52:53 +0200

    Create Alternative Matching.java

---
+++ Grouping and Capturing/Alternative Matching.java
@@ -0,0 +1,10 @@
+// link : https://www.hackerrank.com/challenges/alternative-matching/problem
+public class Solution {    
+
+    public static void main(String[] args) {
+        
+        Regex_Test tester = new Regex_Test();
+        tester.checker("^(Mr|Mrs|Ms|Dr|Er)\\.[a-zA-Z]+$"); // Use \\ instead of using \ 
+    
+    }
+}
