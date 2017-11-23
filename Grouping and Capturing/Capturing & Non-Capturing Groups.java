1e842d89d249ea54ba4ffc824b79def46852fb37
Youssef Ali <youssefali6212@gmail.com>
2017-11-23 08:47:11 +0200

    Create Capturing & Non-Capturing Groups.java

---
+++ Grouping and Capturing/Capturing & Non-Capturing Groups.java
@@ -0,0 +1,10 @@
+// link : https://www.hackerrank.com/challenges/capturing-non-capturing-groups/problem
+public class Solution {    
+
+    public static void main(String[] args) {
+        
+        Regex_Test tester = new Regex_Test();
+        tester.checker("(ok){3,}"); // Use \\ instead of using \ 
+    
+    }
+}
