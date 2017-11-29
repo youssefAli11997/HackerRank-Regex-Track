263156a9a39bd1d4b179aaba317fffcf307a8787
Youssef Ali <youssefali6212@gmail.com>
2017-11-29 08:20:31 +0200

    Create Backreferences To Failed Groups.java

---
+++ Backreferences/Backreferences To Failed Groups.java
@@ -0,0 +1,10 @@
+// link : https://www.hackerrank.com/challenges/backreferences-to-failed-groups/
+public class Solution {    
+
+    public static void main(String[] args) {
+        
+        Regex_Test tester = new Regex_Test();
+        tester.checker("^\\d{2}(-?)\\d{2}\\1\\d{2}\\1\\d{2}$"); // Use \\ instead of using \ 
+    
+    }
+}
