1f9330779d0dbf9cae8782818010f4fd0fa7053b
Youssef Ali <youssefali6212@gmail.com>
2018-01-31 00:00:38 +0200

    Create Negative Lookbehind.java

---
+++ Assertions/Negative Lookbehind.java
@@ -0,0 +1,10 @@
+//link: https://www.hackerrank.com/challenges/negative-lookbehind/problem
+public class Solution {    
+
+    public static void main(String[] args) {
+        
+        Regex_Test tester = new Regex_Test();
+        tester.checker("(?<![aieouAIEOU])."); //Use '\\' instead of '\'.
+    
+    }
+}
