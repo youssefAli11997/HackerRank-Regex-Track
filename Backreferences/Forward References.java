c35b50d46dc167db4a6bc9aa3be42d8493aed21c
Youssef Ali <youssefali6212@gmail.com>
2018-01-30 22:19:47 +0200

    Update Forward References.java

--- Backreferences/Forward References.java
+++ Backreferences/Forward References.java
@@ -4,6 +4,8 @@ public class Solution {
     public static void main(String[] args) {
         
         Regex_Test tester = new Regex_Test();
+        tester.checker("^(\\2tic|(tac))+$"); // Use \\ instead of using \
+        // another solution (without forward references)
         tester.checker("^tac(tac(tic)?)*$"); // Use \\ instead of using \ 
     
     }
