a5fa5873847a332972c66c2d0cd1ccca0641527e
Youssef Ali <youssefali6212@gmail.com>
2018-01-30 22:07:17 +0200

    Create Forward References.java

---
+++ Backreferences/Forward References.java
@@ -0,0 +1,10 @@
+//link: https://www.hackerrank.com/challenges/forward-references/problem
+public class Solution {    
+
+    public static void main(String[] args) {
+        
+        Regex_Test tester = new Regex_Test();
+        tester.checker("^tac(tac(tic)?)*$"); // Use \\ instead of using \ 
+    
+    }
+}
