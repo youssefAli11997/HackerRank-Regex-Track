fa5907cc65c90836746f29c0d2d186202d861d62
Youssef Ali <youssefali6212@gmail.com>
2018-01-31 13:21:25 +0200

    Create Detect_HTML_links.java

---
+++ Applications/Detect_HTML_links.java
@@ -0,0 +1,33 @@
+//link: 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Detect_HTML_links {
+
+    private static Pattern pattern;
+
+    private static void detect(String code){
+        Matcher matcher = pattern.matcher(code);
+        while(matcher.find()){
+            String link = matcher.group(1).trim();
+            String text = matcher.group(2).trim();
+            text = text.replaceAll("<.*?>", "");
+            System.out.println(link + "," + text);
+        }
+    }
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt(); in.nextLine();
+        String htmlCode = "";
+
+        String regex_Pattern = "<a\\shref=\"(.*?)\".*?>(.*?)<\\/a>";
+        pattern = Pattern.compile(regex_Pattern);
+
+        for(int i=0; i<n; i++){
+            htmlCode = in.nextLine();
+            detect(htmlCode);
+        }
+    }
+}
